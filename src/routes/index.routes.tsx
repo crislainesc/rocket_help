b6b8585f4a5ce72bbbc60802eb50bbfc7cf9b3f9
Crislâine Santos <criis.santos@outlook.com>
2022-08-28 10:24:25 -0300

    ✨ add login verification

--- src/routes/index.routes.tsx
+++ src/routes/index.routes.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth'
+
+import { Loading } from '../components/loading'
 
 import { SignIn } from '../screens/signin'
 import { AppRoutes } from './app.routes'
 
+type AuthTypes = FirebaseAuthTypes.User | null
+
 export const Routes: React.FC = () => {
-  return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
-  )
+  const [loading, setLoading] = useState<boolean>(true)
+  const [user, setUser] = useState<AuthTypes>()
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged((response) => {
+      setUser(response)
+      setLoading(false)
+    })
+
+    return subscriber
+  }, [])
+
+  if (loading) {
+    return <Loading />
+  }
+
+  return <NavigationContainer>{user ? <AppRoutes /> : <SignIn />}</NavigationContainer>
 }
